7f3d09effcd9752e67786a9aa7b5d236ab8cfc03
Абылхайыр <abylkhairnurgozhaev@gmail.com>
2024-02-08 19:00:43 +0600

    refresh

---
+++ internal/entities/entities.go
@@ -0,0 +1,10 @@
+package entities
+
+type Currencies struct {
+
+	// all need data to currencies time and min max price of changing for hour
+
+	//we can scrap all data or calculate it later
+
+	//
+}
