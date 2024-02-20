8e9b8600fc4016af7da65d391fd8497527495717
abylkhairnurgozhaev <abylkhairnurgozhaev@gmail.com>
2024-02-20 20:34:59 +0600

    upd

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,14 @@
 package cmd
 
-import "fmt"
+import (
+	"log"
+	"restapi"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	srv := new(restapi.Server)
+	if err := srv.Run("8000"); err != nil {
+		log.Fatalf("error")
+	}
 }
--- internal/entities/entities.go
+++ internal/entities/entities.go
@@ -1,10 +1,34 @@
 package entities
 
-type Currencies struct {
+import "time"
 
-	// all need data to currencies time and min max price of changing for hour
+type CurrencyRate struct {
+	Id               string    `json:"id"`     //old as world
+	Title            string    `json:"title"`  //tenge, ruble, dollar
+	Symbol           string    `json:"symbol"` //KZT,RUB,USD
+	Price            float64   `json:"price"`
+	MaxChangePercent float64   `json:"maxChangePercent"`
+	MinChangePercent float64   `json:"minChangePercent"`
+	CreatedAt        time.Time `json:"createdAt" `
+	UpdatedAt        time.Time `json:"updatedAt"`
+}
 
-	//we can scrap all data or calculate it later
+func NewCurrencyRate(id, title, symbol string, price float64) *CurrencyRate {
+	return &CurrencyRate{
+		Id:               id,
+		Title:            title,
+		Symbol:           symbol,
+		Price:            price,
+		MaxChangePercent: 0,
+		MinChangePercent: 0,
+		CreatedAt:        time.Now(),
+		UpdatedAt:        time.Now(),
+	}
+}
 
-	//
+// all need data to currencies time and min max price of changing for hour
+// we can scrap all data or calculate it later
+func calculate() {
+	//take data from api and calculate
+	//orr take already calculated data
 }
