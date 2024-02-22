80eff30e5745a0cad47230da2cf83fc3508d811b
abylkhairnurgozhaev <abylkhairnurgozhaev@gmail.com>
2024-02-22 17:33:10 +0600

    wait

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,18 @@ package cmd
 import (
 	"log"
 	"restapi"
+	"restapi/pkg/handler"
+	"restapi/pkg/repository"
+	"restapi/pkg/service"
 )
 
 func main() {
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(restapi.Server)
-	if err := srv.Run("8000"); err != nil {
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error")
 	}
 }
