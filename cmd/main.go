13e13a8e55b1a7c3260330e175d05fb682a2d82b
abylkhairnurgozhaev <abylkhairnurgozhaev@gmail.com>
2024-02-23 04:00:03 +0600

    migrate

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
-package cmd
+package main
 
 import (
+	"github.com/spf13/viper"
 	"log"
 	"restapi"
 	"restapi/pkg/handler"
@@ -9,12 +10,22 @@ import (
 )
 
 func main() {
+	err := initConfig()
+	if err != nil {
+		log.Fatalf("error init of config: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(restapi.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error")
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
---
+++ schema/000001_init.up.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users
+(
+    id serial not null unique
+    name varchar(255)
+)
